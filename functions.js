8230cef728a6154e294e49a01a1870ece2201ece
irfanbaigse <irfan.blackhawk@gmail.com>
2019-09-16 15:39:17 +0400

    jest unit testing crash testing

---
+++ functions.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+const functions = {
+  add: (num1, num2) => {
+    return num1 + num2;
+  },
+  isNull: () => {return null},
+
+  fetchUser: () => {
+     return axios.get('https://jsonplaceholder.typicode.com/users/1').then(res => res.data).catch(err => 'error');
+  }
+};
+
+module.exports = functions;
---
+++ functions.test.js
@@ -0,0 +1,52 @@
+const functions = require('./functions');
+
+// beforeEach(()=> initDB());
+// afterEach(()=> closeDB());
+
+// beforeAll(()=> initDB());
+// afterAll(()=> closeDB());
+
+// const initDB = () => console.log('init db');
+// const closeDB = () => console.log('close db');
+
+const nameCheck = () => console.log('Chekcing Name');
+describe('Check Names', ()=>{
+  beforeEach(()=> nameCheck());
+
+  test('User is jeff', ()=> {
+    expect('jeff').toBe('jeff');
+  })
+
+  test('User is karin', ()=> {
+    expect('karin').toBe('karin');
+  })
+
+  test('User is xxxx', ()=> {
+    expect('xxxx').toBe('xxxx');
+  })
+})
+
+test('Add 2+2', () => {
+  expect(functions.add(2,2)).toBe(4);
+});
+
+test('Not 5', () => {
+  expect(functions.add(2,2)).not.toBe(5);
+});
+
+test('Is Null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test('user Name Leanne Graham', () => {
+  expect.assertions(1);
+   return functions.fetchUser().then(data => {
+    expect(data.name).toEqual('Leanne Graham');
+  });
+});
+
+test('user Name Leanne Graham', async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
+});
